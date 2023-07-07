22abeafe5c9f8e355f62a30bfb61923f70fc5f53
= <=>
2023-07-07 23:57:21 +1000

    Quiz1 Done

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,17 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples (quanitity: i32) -> i32 {
+    if quanitity <= 40 {
+        quanitity *2
+    } else {
+        quanitity
+    }
+
+}
 
 // Don't modify this function!
 #[test]
